d95f7a93948f70656905c80ecb54c903dc60aac6
Navruzbek12 <navruzbek.bobobekov@icloud.com>
2026-02-04 18:16:26 +0500

    Add referral router to setup_routers function

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -5,7 +5,7 @@ from .payment import router as payment_router
 from .admin import router as admin_router
 from .keys import router as keys_router
 from .help import router as help_router
-
+from .referral import router as referral_router 
 def setup_routers():
     """Barcha routerlarni birlashtirish"""
     router = Router()
@@ -16,4 +16,5 @@ def setup_routers():
     router.include_router(admin_router)
     router.include_router(keys_router)
     router.include_router(help_router)
+    router.include_router(referral_router)
     return router
